512fb3166ec88ae069bab1c62636d7c8711ae0d9
Tdragon77 <wheart.dragon@gmail.com>
2022-06-17 17:19:51 +0900

    로그인 구현

---
+++ app/src/main/java/woori/project/newchallenge/MainActivity.java
@@ -0,0 +1,15 @@
+package woori.project.newchallenge;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
